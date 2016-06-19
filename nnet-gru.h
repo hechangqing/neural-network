76007ff1089b157d06c2a47dbcf8cd00135e9db9
hechangqing <494268393@qq.com>
2016-06-19 22:36:36 +0800

    first

--- nnet-gru.h
+++ nnet-gru.h
@@ -420,96 +420,29 @@ public:
 
         if (DEBUG) {
             std::cerr << "gradients(with optional momentum): \n";
-            std::cerr << "w_gifo_x_corr_ " << w_gifo_x_corr_;
-            std::cerr << "w_gifo_r_corr_ " << w_gifo_r_corr_;
-            std::cerr << "bias_corr_ " << bias_corr_;
-            std::cerr << "w_r_m_corr_ " << w_r_m_corr_;
-            std::cerr << "peephole_i_c_corr_ " << peephole_i_c_corr_;
-            std::cerr << "peephole_f_c_corr_ " << peephole_f_c_corr_;
-            std::cerr << "peephole_o_c_corr_ " << peephole_o_c_corr_;
+            std::cerr << "w_z_x_corr_ " << w_z_x_corr_;
+            std::cerr << "w_z_h_corr_ " << w_z_h_corr_;
+            std::cerr << "w_r_x_corr_ " << w_r_x_corr_;
+            std::cerr << "w_r_h_corr_ " << w_r_h_corr_;
+            std::cerr << "w_m_g_corr_ " << w_m_g_corr_;
+            std::cerr << "w_m_x_corr_ " << w_m_x_corr_;
         }
     }
 
     void Update(const CuMatrixBase<BaseFloat> &input, const CuMatrixBase<BaseFloat> &diff) {
         const BaseFloat lr  = opts_.learn_rate;
+        
+        w_z_x_.AddMat(-lr, w_z_x_corr_);
+        w_z_h_.AddMat(-lr, w_z_h_corr_);
+        w_r_x_.AddMat(-lr, w_r_x_corr_);
+        w_r_h_.AddMat(-lr, w_r_h_corr_);
+        w_m_g_.AddMat(-lr, w_m_g_corr_);
+        w_m_x_.AddMat(-lr, w_m_x_corr_);
 
-        w_gifo_x_.AddMat(-lr, w_gifo_x_corr_);
-        w_gifo_r_.AddMat(-lr, w_gifo_r_corr_);
-        bias_.AddVec(-lr, bias_corr_, 1.0);
-    
-        peephole_i_c_.AddVec(-lr, peephole_i_c_corr_, 1.0);
-        peephole_f_c_.AddVec(-lr, peephole_f_c_corr_, 1.0);
-        peephole_o_c_.AddVec(-lr, peephole_o_c_corr_, 1.0);
-    
-        w_r_m_.AddMat(-lr, w_r_m_corr_);
-
-//        /* 
-//          Here we deal with the famous "vanishing & exploding difficulties" in RNN learning.
-//
-//          *For gradients vanishing*
-//            LSTM architecture introduces linear CEC as the "error bridge" across long time distance
-//            solving vanishing problem.
-//
-//          *For gradients exploding*
-//            LSTM is still vulnerable to gradients explosing in BPTT(with large weight & deep time expension).
-//            To prevent this, we tried L2 regularization, which didn't work well
-//
-//          Our approach is a *modified* version of Max Norm Regularization:
-//          For each nonlinear neuron, 
-//            1. fan-in weights & bias model a seperation hyper-plane: W x + b = 0
-//            2. squashing function models a differentiable nonlinear slope around this hyper-plane.
-//
-//          Conventional max norm regularization scale W to keep its L2 norm bounded,
-//          As a modification, we scale down large (W & b) *simultaneously*, this:
-//            1. keeps all fan-in weights small, prevents gradients from exploding during backward-pass.
-//            2. keeps the location of the hyper-plane unchanged, so we don't wipe out already learned knowledge.
-//            3. shrinks the "normal" of the hyper-plane, smooths the nonlinear slope, improves generalization.
-//            4. makes the network *well-conditioned* (weights are constrained in a reasonible range).
-//
-//          We've observed faster convergence and performance gain by doing this.
-//        */
-//
-//        int DEBUG = 0;
-//        BaseFloat max_norm = 1.0;   // weights with large L2 norm may cause exploding in deep BPTT expensions
-//                                    // TODO: move this config to opts_
-//        CuMatrix<BaseFloat> L2_gifo_x(w_gifo_x_);
-//        CuMatrix<BaseFloat> L2_gifo_r(w_gifo_r_);
-//        L2_gifo_x.MulElements(w_gifo_x_);
-//        L2_gifo_r.MulElements(w_gifo_r_);
-//
-//        CuVector<BaseFloat> L2_norm_gifo(L2_gifo_x.NumRows());
-//        L2_norm_gifo.AddColSumMat(1.0, L2_gifo_x, 0.0);
-//        L2_norm_gifo.AddColSumMat(1.0, L2_gifo_r, 1.0);
-//        L2_norm_gifo.Range(1*ncell_, ncell_).AddVecVec(1.0, peephole_i_c_, peephole_i_c_, 1.0);
-//        L2_norm_gifo.Range(2*ncell_, ncell_).AddVecVec(1.0, peephole_f_c_, peephole_f_c_, 1.0);
-//        L2_norm_gifo.Range(3*ncell_, ncell_).AddVecVec(1.0, peephole_o_c_, peephole_o_c_, 1.0);
-//        L2_norm_gifo.ApplyPow(0.5);
-//
-//        CuVector<BaseFloat> shrink(L2_norm_gifo);
-//        shrink.Scale(1.0/max_norm);
-//        shrink.ApplyFloor(1.0);
-//        shrink.InvertElements();
-//
-//        w_gifo_x_.MulRowsVec(shrink);
-//        w_gifo_r_.MulRowsVec(shrink);
-//        bias_.MulElements(shrink);
-//
-//        peephole_i_c_.MulElements(shrink.Range(1*ncell_, ncell_));
-//        peephole_f_c_.MulElements(shrink.Range(2*ncell_, ncell_));
-//        peephole_o_c_.MulElements(shrink.Range(3*ncell_, ncell_));
-//
-//        if (DEBUG) {
-//            if (shrink.Min() < 0.95) {   // we dont want too many trivial logs here
-//                std::cerr << "gifo shrinking coefs: " << shrink;
-//            }
-//        }
-//        
     }
 
 private:
     // dims
-    int32 ncell_;
-    int32 nrecur_;  // recurrent projection layer dim
     int32 nstream_;
 
     CuMatrix<BaseFloat> prev_nnet_state_;
@@ -517,40 +450,22 @@ private:
     // gradient-clipping value,
     BaseFloat clip_gradient_;
 
-    // non-recurrent dropout 
-    //BaseFloat dropout_rate_;
-    //CuMatrix<BaseFloat> dropout_mask_;
+    // feed-forward connections: from x to [z, r, m]
+    CuMatrix<BaseFloat> w_z_x_;
+    CuMatrix<BaseFloat> w_r_x_;
+    CuMatrix<BaseFloat> w_m_x_;
 
-    // feed-forward connections: from x to [g, i, f, o]
-    CuMatrix<BaseFloat> w_gifo_x_;
-    CuMatrix<BaseFloat> w_gifo_x_corr_;
+    // recurrent connections: from h, g to [z, r, m]
+    CuMatrix<BaseFloat> w_z_h_;
+    CuMatrix<BaseFloat> w_r_h_;
+    CuMatrix<BaseFloat> w_m_g_;
 
-    // recurrent projection connections: from r to [g, i, f, o]
-    CuMatrix<BaseFloat> w_gifo_r_;
-    CuMatrix<BaseFloat> w_gifo_r_corr_;
-
-    // biases of [g, i, f, o]
-    CuVector<BaseFloat> bias_;
-    CuVector<BaseFloat> bias_corr_;
-
-    // peephole from c to i, f, g 
-    // peephole connections are block-internal, so we use vector form
-    CuVector<BaseFloat> peephole_i_c_;
-    CuVector<BaseFloat> peephole_f_c_;
-    CuVector<BaseFloat> peephole_o_c_;
-
-    CuVector<BaseFloat> peephole_i_c_corr_;
-    CuVector<BaseFloat> peephole_f_c_corr_;
-    CuVector<BaseFloat> peephole_o_c_corr_;
-
-    // projection layer r: from m to r
-    CuMatrix<BaseFloat> w_r_m_;
     CuMatrix<BaseFloat> w_r_m_corr_;
 
-    // propagate buffer: output of [g, i, f, o, c, h, m, r]
+    // propagate buffer: output of [z, r, g, m, h]
     CuMatrix<BaseFloat> propagate_buf_;
 
-    // back-propagate buffer: diff-input of [g, i, f, o, c, h, m, r]
+    // back-propagate buffer: diff-input of [z, r, g, m, h]
     CuMatrix<BaseFloat> backpropagate_buf_;
 
 };
