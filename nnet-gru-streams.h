49eedf375d69b37a8d469d906236f3be0e32623a
hechangqing <494268393@qq.com>
2016-06-20 22:42:50 +0800

    version 2; more faster

--- nnet-gru-streams.h
+++ nnet-gru-streams.h
@@ -7,6 +7,7 @@
 
 /*************************************
  * x: input neuron
+
  *************************************/
 
 namespace kaldi {
@@ -60,26 +61,17 @@ public:
         }
 
         // init weight (Uniform)
-        w_z_x_.Resize(output_dim_, input_dim_, kUndefined);
-        w_r_x_.Resize(output_dim_, input_dim_, kUndefined);
-        w_z_h_.Resize(output_dim_, output_dim_, kUndefined);
-        w_r_h_.Resize(output_dim_, output_dim_, kUndefined);
-        w_m_x_.Resize(output_dim_, input_dim_, kUndefined);
+        w_zrm_x_.Resize(3*output_dim_, input_dim_, kUndefined);
+        w_zr_h_.Resize(2*output_dim_, output_dim_, kUndefined);
         w_m_g_.Resize(output_dim_, output_dim_, kUndefined);
 
-        InitMatParam(w_z_x_, param_scale);
-        InitMatParam(w_r_x_, param_scale);
-        InitMatParam(w_z_h_, param_scale);
-        InitMatParam(w_r_h_, param_scale);
-        InitMatParam(w_m_x_, param_scale);
+        InitMatParam(w_zrm_x_, param_scale);
+        InitMatParam(w_zr_h_, param_scale);
         InitMatParam(w_m_g_, param_scale);
 
         // init delta buffers
-        w_z_x_corr_.Resize(output_dim_, input_dim_, kSetZero); 
-        w_r_x_corr_.Resize(output_dim_, input_dim_, kSetZero); 
-        w_z_h_corr_.Resize(output_dim_, output_dim_, kSetZero); 
-        w_r_h_corr_.Resize(output_dim_, output_dim_, kSetZero); 
-        w_m_x_corr_.Resize(output_dim_, input_dim_, kSetZero); 
+        w_zrm_x_corr_.Resize(3*output_dim_, input_dim_, kSetZero); 
+        w_zr_h_corr_.Resize(2*output_dim_, output_dim_, kSetZero); 
         w_m_g_corr_.Resize(output_dim_, output_dim_, kSetZero); 
 
         KALDI_ASSERT(clip_gradient_ >= 0.0);
@@ -91,18 +83,12 @@ public:
         //ExpectToken(is, binary, "<DropoutRate>");
         //ReadBasicType(is, binary, &dropout_rate_);
         
-        w_z_x_.Read(is, binary);
-        w_r_x_.Read(is, binary);
-        w_z_h_.Read(is, binary);
-        w_r_h_.Read(is, binary);
-        w_m_x_.Read(is, binary);
+        w_zrm_x_.Read(is, binary);
+        w_zr_h_.Read(is, binary);
         w_m_g_.Read(is, binary);
 
-        w_z_x_corr_.Resize(output_dim_, input_dim_, kSetZero); 
-        w_r_x_corr_.Resize(output_dim_, input_dim_, kSetZero); 
-        w_z_h_corr_.Resize(output_dim_, output_dim_, kSetZero); 
-        w_r_h_corr_.Resize(output_dim_, output_dim_, kSetZero); 
-        w_m_x_corr_.Resize(output_dim_, input_dim_, kSetZero); 
+        w_zrm_x_corr_.Resize(3*output_dim_, input_dim_, kSetZero); 
+        w_zr_h_corr_.Resize(2*output_dim_, output_dim_, kSetZero); 
         w_m_g_corr_.Resize(output_dim_, output_dim_, kSetZero); 
     }
 
@@ -111,20 +97,14 @@ public:
         WriteBasicType(os, binary, clip_gradient_);
         //WriteToken(os, binary, "<DropoutRate>");
         //WriteBasicType(os, binary, dropout_rate_);
-        w_z_x_.Write(os, binary);
-        w_r_x_.Write(os, binary);
-        w_z_h_.Write(os, binary);
-        w_r_h_.Write(os, binary);
-        w_m_x_.Write(os, binary);
+        w_zrm_x_.Write(os, binary);
+        w_zr_h_.Write(os, binary);
         w_m_g_.Write(os, binary);
     }
 
     int32 NumParams() const { 
-        return ( w_z_x_.NumRows() * w_z_x_.NumCols() +
-                 w_r_x_.NumRows() * w_r_x_.NumCols() +
-                 w_z_h_.NumRows() * w_z_h_.NumCols() +
-                 w_r_h_.NumRows() * w_r_h_.NumCols() +
-                 w_m_x_.NumRows() * w_m_x_.NumCols() +
+        return ( w_zrm_x_.NumRows() * w_zrm_x_.NumCols() +
+                 w_zr_h_.NumRows() * w_zr_h_.NumCols() +
                  w_m_g_.NumRows() * w_m_g_.NumCols() );
     }
 
@@ -133,20 +113,11 @@ public:
 
         int32 offset, len;
 
-        offset = 0;    len = w_z_x_.NumRows() * w_z_x_.NumCols();
-        wei_copy->Range(offset, len).CopyRowsFromMat(w_z_x_);
+        offset = 0;    len = w_zrm_x_.NumRows() * w_zrm_x_.NumCols();
+        wei_copy->Range(offset, len).CopyRowsFromMat(w_zrm_x_);
 
-        offset += len; len = w_r_x_.NumRows() * w_r_x_.NumCols();
-        wei_copy->Range(offset, len).CopyRowsFromMat(w_r_x_);
-
-        offset += len; len = w_z_h_.NumRows() * w_z_h_.NumCols();
-        wei_copy->Range(offset, len).CopyRowsFromMat(w_z_h_);
-
-        offset += len; len = w_r_h_.NumRows() * w_r_h_.NumCols();
-        wei_copy->Range(offset, len).CopyRowsFromMat(w_r_h_);
-
-        offset += len; len = w_m_x_.NumRows() * w_m_x_.NumCols();
-        wei_copy->Range(offset, len).CopyRowsFromMat(w_m_x_);
+        offset += len; len = w_zr_h_.NumRows() * w_zr_h_.NumCols();
+        wei_copy->Range(offset, len).CopyRowsFromMat(w_zr_h_);
 
         offset += len; len = w_m_g_.NumRows() * w_m_g_.NumCols();
         wei_copy->Range(offset, len).CopyRowsFromMat(w_m_g_);
@@ -156,11 +127,8 @@ public:
 
     std::string Info() const {
         return std::string("    ") + 
-            "\n  w_z_x_  "     + MomentStatistics(w_z_x_) + 
-            "\n  w_r_x_  "     + MomentStatistics(w_r_x_) +
-            "\n  w_z_h_  "     + MomentStatistics(w_z_h_) +
-            "\n  w_r_h_  "     + MomentStatistics(w_r_h_) +
-            "\n  w_m_x_  "     + MomentStatistics(w_m_x_) +
+            "\n  w_zrm_x_  "     + MomentStatistics(w_zrm_x_) + 
+            "\n  w_zr_h_  "     + MomentStatistics(w_zr_h_) +
             "\n  w_m_g_  "     + MomentStatistics(w_m_g_);
     }
   
@@ -168,36 +136,33 @@ public:
         // disassemble forward-propagation buffer into different neurons,
         const CuSubMatrix<BaseFloat> YZ(propagate_buf_.ColRange(0*output_dim_, output_dim_));
         const CuSubMatrix<BaseFloat> YR(propagate_buf_.ColRange(1*output_dim_, output_dim_));
-        const CuSubMatrix<BaseFloat> YG(propagate_buf_.ColRange(2*output_dim_, output_dim_));
-        const CuSubMatrix<BaseFloat> YM(propagate_buf_.ColRange(3*output_dim_, output_dim_));
+        const CuSubMatrix<BaseFloat> YM(propagate_buf_.ColRange(2*output_dim_, output_dim_));
+        const CuSubMatrix<BaseFloat> YG(propagate_buf_.ColRange(3*output_dim_, output_dim_));
         const CuSubMatrix<BaseFloat> YH(propagate_buf_.ColRange(4*output_dim_, output_dim_));
 
         // disassemble backpropagate buffer into different neurons,
         const CuSubMatrix<BaseFloat> DZ(backpropagate_buf_.ColRange(0*output_dim_, output_dim_));
         const CuSubMatrix<BaseFloat> DR(backpropagate_buf_.ColRange(1*output_dim_, output_dim_));
-        const CuSubMatrix<BaseFloat> DG(backpropagate_buf_.ColRange(2*output_dim_, output_dim_));
-        const CuSubMatrix<BaseFloat> DM(backpropagate_buf_.ColRange(3*output_dim_, output_dim_));
+        const CuSubMatrix<BaseFloat> DM(backpropagate_buf_.ColRange(2*output_dim_, output_dim_));
+        const CuSubMatrix<BaseFloat> DG(backpropagate_buf_.ColRange(3*output_dim_, output_dim_));
         const CuSubMatrix<BaseFloat> DH(backpropagate_buf_.ColRange(4*output_dim_, output_dim_));
 
         return std::string("    ") + 
             "\n  Gradients:" +
-            "\n    w_z_x_corr_  "     + MomentStatistics(w_z_x_corr_) + 
-            "\n    w_r_x_corr_  "     + MomentStatistics(w_r_x_corr_) +
-            "\n    w_z_h_corr_  "     + MomentStatistics(w_z_h_corr_) +
-            "\n    w_r_h_corr_  "     + MomentStatistics(w_r_h_corr_) +
-            "\n    w_m_x_corr_  "     + MomentStatistics(w_m_x_corr_) +
+            "\n    w_zrm_x_corr_  "     + MomentStatistics(w_zrm_x_corr_) + 
+            "\n    w_zr_h_corr_  "     + MomentStatistics(w_zr_h_corr_) +
             "\n    w_m_g_corr_  "     + MomentStatistics(w_m_g_corr_) +
             "\n  Forward-pass:" +
             "\n    YZ  " + MomentStatistics(YZ) +
             "\n    YR  " + MomentStatistics(YR) +
-            "\n    YG  " + MomentStatistics(YG) +
             "\n    YM  " + MomentStatistics(YM) +
+            "\n    YG  " + MomentStatistics(YG) +
             "\n    YH  " + MomentStatistics(YH) +
             "\n  Backward-pass:" +
             "\n    DZ  " + MomentStatistics(DZ) +
             "\n    DR  " + MomentStatistics(DR) +
-            "\n    DG  " + MomentStatistics(DG) +
             "\n    DM  " + MomentStatistics(DM) +
+            "\n    DG  " + MomentStatistics(DG) +
             "\n    DH  " + MomentStatistics(DH);
     }
 
@@ -242,30 +207,30 @@ public:
         // disassemble entire neuron activation buffer into different neurons
         CuSubMatrix<BaseFloat> YZ(propagate_buf_.ColRange(0*output_dim_, output_dim_));
         CuSubMatrix<BaseFloat> YR(propagate_buf_.ColRange(1*output_dim_, output_dim_));
-        CuSubMatrix<BaseFloat> YG(propagate_buf_.ColRange(2*output_dim_, output_dim_));
-        CuSubMatrix<BaseFloat> YM(propagate_buf_.ColRange(3*output_dim_, output_dim_));
+        CuSubMatrix<BaseFloat> YM(propagate_buf_.ColRange(2*output_dim_, output_dim_));
+        CuSubMatrix<BaseFloat> YG(propagate_buf_.ColRange(3*output_dim_, output_dim_));
         CuSubMatrix<BaseFloat> YH(propagate_buf_.ColRange(4*output_dim_, output_dim_));
 
+        CuSubMatrix<BaseFloat> YZRM(propagate_buf_.ColRange(0, 3*output_dim_));
+        CuSubMatrix<BaseFloat> YZR(propagate_buf_.ColRange(0, 2*output_dim_));
         // x -> z, r, m, not recurrent, do it all in once
-        YZ.RowRange(1*S,T*S).AddMatMat(1.0, in, kNoTrans, w_z_x_, kTrans, 0.0);
-        YR.RowRange(1*S,T*S).AddMatMat(1.0, in, kNoTrans, w_r_x_, kTrans, 0.0);
-        YM.RowRange(1*S,T*S).AddMatMat(1.0, in, kNoTrans, w_m_x_, kTrans, 0.0);
+        YZRM.RowRange(1*S,T*S).AddMatMat(1.0, in, kNoTrans, w_zrm_x_, kTrans, 0.0);
 
         for (int t = 1; t <= T; t++) {
             // multistream buffers for current time-step
             CuSubMatrix<BaseFloat> y_z(YZ.RowRange(t*S,S));  
             CuSubMatrix<BaseFloat> y_r(YR.RowRange(t*S,S));  
-            CuSubMatrix<BaseFloat> y_g(YG.RowRange(t*S,S));  
             CuSubMatrix<BaseFloat> y_m(YM.RowRange(t*S,S));  
+            CuSubMatrix<BaseFloat> y_g(YG.RowRange(t*S,S));  
             CuSubMatrix<BaseFloat> y_h(YH.RowRange(t*S,S));  
+            
+            CuSubMatrix<BaseFloat> y_zr(YZR.RowRange(t*S,S));  
     
             // h(t-1) -> z, r
-            y_z.AddMatMat(1.0, YH.RowRange((t-1)*S,S), kNoTrans, w_z_h_, kTrans,  1.0);
-            y_r.AddMatMat(1.0, YH.RowRange((t-1)*S,S), kNoTrans, w_r_h_, kTrans,  1.0);
+            y_zr.AddMatMat(1.0, YH.RowRange((t-1)*S,S), kNoTrans, w_zr_h_, kTrans,  1.0);
 
             // z, r sigmoid squashing
-            y_z.Sigmoid(y_z);
-            y_r.Sigmoid(y_r);
+            y_zr.Sigmoid(y_zr);
 
             // r(t) * h(t-1) -> g(t)
             y_g.AddMatMatElements(1.0, YH.RowRange((t-1)*S,S), y_r, 0.0);
@@ -285,8 +250,8 @@ public:
                 std::cerr << "forward-pass frame " << t << "\n";
                 std::cerr << "activation of z: " << y_z;
                 std::cerr << "activation of r: " << y_r;
-                std::cerr << "activation of g: " << y_g;
                 std::cerr << "activation of m: " << y_m;
+                std::cerr << "activation of g: " << y_g;
                 std::cerr << "activation of h: " << y_h;
             }
         }
@@ -309,8 +274,8 @@ public:
         // disassemble propagated buffer into neurons
         CuSubMatrix<BaseFloat> YZ(propagate_buf_.ColRange(0*output_dim_, output_dim_));
         CuSubMatrix<BaseFloat> YR(propagate_buf_.ColRange(1*output_dim_, output_dim_));
-        CuSubMatrix<BaseFloat> YG(propagate_buf_.ColRange(2*output_dim_, output_dim_));
-        CuSubMatrix<BaseFloat> YM(propagate_buf_.ColRange(3*output_dim_, output_dim_));
+        CuSubMatrix<BaseFloat> YM(propagate_buf_.ColRange(2*output_dim_, output_dim_));
+        CuSubMatrix<BaseFloat> YG(propagate_buf_.ColRange(3*output_dim_, output_dim_));
         CuSubMatrix<BaseFloat> YH(propagate_buf_.ColRange(4*output_dim_, output_dim_));
     
         // 0:dummy, [1,T] frames, T+1 backward pass history
@@ -319,9 +284,12 @@ public:
         // disassemble backpropagate buffer into neurons
         CuSubMatrix<BaseFloat> DZ(backpropagate_buf_.ColRange(0*output_dim_, output_dim_));
         CuSubMatrix<BaseFloat> DR(backpropagate_buf_.ColRange(1*output_dim_, output_dim_));
-        CuSubMatrix<BaseFloat> DG(backpropagate_buf_.ColRange(2*output_dim_, output_dim_));
-        CuSubMatrix<BaseFloat> DM(backpropagate_buf_.ColRange(3*output_dim_, output_dim_));
+        CuSubMatrix<BaseFloat> DM(backpropagate_buf_.ColRange(2*output_dim_, output_dim_));
+        CuSubMatrix<BaseFloat> DG(backpropagate_buf_.ColRange(3*output_dim_, output_dim_));
         CuSubMatrix<BaseFloat> DH(backpropagate_buf_.ColRange(4*output_dim_, output_dim_));
+        
+        CuSubMatrix<BaseFloat> DZRM(backpropagate_buf_.ColRange(0, 3*output_dim_));
+        CuSubMatrix<BaseFloat> DZR(backpropagate_buf_.ColRange(0, 2*output_dim_));
 
         // backprop it all in once
         DH.RowRange(1*S,T*S).CopyFromMat(out_diff);
@@ -329,19 +297,18 @@ public:
         for (int t = T; t >= 1; t--) {
             CuSubMatrix<BaseFloat> y_z(YZ.RowRange(t*S,S));  
             CuSubMatrix<BaseFloat> y_r(YR.RowRange(t*S,S));  
-            CuSubMatrix<BaseFloat> y_g(YG.RowRange(t*S,S));  
             CuSubMatrix<BaseFloat> y_m(YM.RowRange(t*S,S));  
+            CuSubMatrix<BaseFloat> y_g(YG.RowRange(t*S,S));  
             CuSubMatrix<BaseFloat> y_h(YH.RowRange(t*S,S));  
     
             CuSubMatrix<BaseFloat> d_z(DZ.RowRange(t*S,S));
             CuSubMatrix<BaseFloat> d_r(DR.RowRange(t*S,S));
-            CuSubMatrix<BaseFloat> d_g(DG.RowRange(t*S,S));
             CuSubMatrix<BaseFloat> d_m(DM.RowRange(t*S,S));
+            CuSubMatrix<BaseFloat> d_g(DG.RowRange(t*S,S));
             CuSubMatrix<BaseFloat> d_h(DH.RowRange(t*S,S));
             
             // h
-            d_h.AddMatMat(1.0, DR.RowRange((t+1)*S,S), kNoTrans, w_r_h_, kNoTrans, 1.0);
-            d_h.AddMatMat(1.0, DZ.RowRange((t+1)*S,S), kNoTrans, w_z_h_, kNoTrans, 1.0);
+            d_h.AddMatMat(1.0, DZR.RowRange((t+1)*S,S), kNoTrans, w_zr_h_, kNoTrans, 1.0);
             d_h.AddMat(1.0, DH.RowRange((t+1)*S,S));
             d_h.AddMatMatElements(-1.0, DH.RowRange((t+1)*S,S), YZ.RowRange((t+1)*S,S), 1.0);
             d_h.AddMatMatElements(1.0, DG.RowRange((t+1)*S,S), YR.RowRange((t+1)*S,S), 1.0);
@@ -367,16 +334,14 @@ public:
                 std::cerr << "backward-pass frame " << t << "\n";
                 std::cerr << "derivative wrt input z " << d_z;
                 std::cerr << "derivative wrt input r " << d_r;
-                std::cerr << "derivative wrt input g " << d_g;
                 std::cerr << "derivative wrt input m " << d_m;
+                std::cerr << "derivative wrt input g " << d_g;
                 std::cerr << "derivative wrt input h " << d_h;
             }
         }
         
         // z,r,m -> x, do it all in once
-        in_diff->AddMatMat(1.0, DZ.RowRange(1*S,T*S), kNoTrans, w_z_x_, kNoTrans, 0.0);
-        in_diff->AddMatMat(1.0, DR.RowRange(1*S,T*S), kNoTrans, w_r_x_, kNoTrans, 1.0);
-        in_diff->AddMatMat(1.0, DM.RowRange(1*S,T*S), kNoTrans, w_m_x_, kNoTrans, 1.0);
+        in_diff->AddMatMat(1.0, DZRM.RowRange(1*S,T*S), kNoTrans, w_zrm_x_, kNoTrans, 0.0);
         //// backward pass dropout
         //if (dropout_rate_ != 0.0) {
         //    in_diff->MulElements(dropout_mask_);
@@ -385,59 +350,39 @@ public:
         // calculate delta
         const BaseFloat mmt = opts_.momentum;
     
-        // weight x -> z
-        w_z_x_corr_.AddMatMat(1.0, DZ.RowRange(1*S,T*S), kTrans, 
-                                   in                  , kNoTrans, mmt);
-        // weight x -> r
-        w_r_x_corr_.AddMatMat(1.0, DR.RowRange(1*S,T*S), kTrans, 
-                                   in                  , kNoTrans, mmt);
-        // weight x -> m
-        w_m_x_corr_.AddMatMat(1.0, DM.RowRange(1*S,T*S), kTrans, 
-                                   in                  , kNoTrans, mmt);
+        // weight x -> z,r,m
+        w_zrm_x_corr_.AddMatMat(1.0, DZRM.RowRange(1*S,T*S), kTrans, 
+                                     in                    , kNoTrans, mmt);
         // recurrent weight h -> z, r
-        w_z_h_corr_.AddMatMat(1.0, DZ.RowRange(1*S,T*S), kTrans, 
-                                   YH.RowRange(0*S,T*S), kNoTrans, mmt);
-        w_r_h_corr_.AddMatMat(1.0, DR.RowRange(1*S,T*S), kTrans, 
-                                   YH.RowRange(0*S,T*S), kNoTrans, mmt);
+        w_zr_h_corr_.AddMatMat(1.0, DZR.RowRange(1*S,T*S), kTrans, 
+                                    YH.RowRange(0*S,T*S) , kNoTrans, mmt);
         // recurrent weight g -> m
         w_m_g_corr_.AddMatMat(1.0, DM.RowRange(1*S,T*S), kTrans, 
                                    YG.RowRange(1*S,T*S), kNoTrans, mmt);
 
         if (clip_gradient_ > 0.0) {
-          w_z_x_corr_.ApplyFloor(-clip_gradient_);
-          w_z_x_corr_.ApplyCeiling(clip_gradient_);
-          w_r_x_corr_.ApplyFloor(-clip_gradient_);
-          w_r_x_corr_.ApplyCeiling(clip_gradient_);
-          w_m_x_corr_.ApplyFloor(-clip_gradient_);
-          w_m_x_corr_.ApplyCeiling(clip_gradient_);
-          w_z_h_corr_.ApplyFloor(-clip_gradient_);
-          w_z_h_corr_.ApplyCeiling(clip_gradient_);
-          w_r_h_corr_.ApplyFloor(-clip_gradient_);
-          w_r_h_corr_.ApplyCeiling(clip_gradient_);
+          w_zrm_x_corr_.ApplyFloor(-clip_gradient_);
+          w_zrm_x_corr_.ApplyCeiling(clip_gradient_);
+          w_zr_h_corr_.ApplyFloor(-clip_gradient_);
+          w_zr_h_corr_.ApplyCeiling(clip_gradient_);
           w_m_g_corr_.ApplyFloor(-clip_gradient_);
           w_m_g_corr_.ApplyCeiling(clip_gradient_);
         }
 
         if (DEBUG) {
             std::cerr << "gradients(with optional momentum): \n";
-            std::cerr << "w_z_x_corr_ " << w_z_x_corr_;
-            std::cerr << "w_z_h_corr_ " << w_z_h_corr_;
-            std::cerr << "w_r_x_corr_ " << w_r_x_corr_;
-            std::cerr << "w_r_h_corr_ " << w_r_h_corr_;
+            std::cerr << "w_zrm_x_corr_ " << w_zrm_x_corr_;
+            std::cerr << "w_zr_h_corr_ " << w_zr_h_corr_;
             std::cerr << "w_m_g_corr_ " << w_m_g_corr_;
-            std::cerr << "w_m_x_corr_ " << w_m_x_corr_;
         }
     }
 
     void Update(const CuMatrixBase<BaseFloat> &input, const CuMatrixBase<BaseFloat> &diff) {
         const BaseFloat lr  = opts_.learn_rate;
         
-        w_z_x_.AddMat(-lr, w_z_x_corr_);
-        w_z_h_.AddMat(-lr, w_z_h_corr_);
-        w_r_x_.AddMat(-lr, w_r_x_corr_);
-        w_r_h_.AddMat(-lr, w_r_h_corr_);
+        w_zrm_x_.AddMat(-lr, w_zrm_x_corr_);
+        w_zr_h_.AddMat(-lr, w_zr_h_corr_);
         w_m_g_.AddMat(-lr, w_m_g_corr_);
-        w_m_x_.AddMat(-lr, w_m_x_corr_);
 
     }
 
@@ -451,20 +396,14 @@ private:
     BaseFloat clip_gradient_;
 
     // feed-forward connections: from x to [z, r, m]
-    CuMatrix<BaseFloat> w_z_x_;
-    CuMatrix<BaseFloat> w_r_x_;
-    CuMatrix<BaseFloat> w_m_x_;
+    CuMatrix<BaseFloat> w_zrm_x_;
 
     // recurrent connections: from h, g to [z, r, m]
-    CuMatrix<BaseFloat> w_z_h_;
-    CuMatrix<BaseFloat> w_r_h_;
+    CuMatrix<BaseFloat> w_zr_h_;
     CuMatrix<BaseFloat> w_m_g_;
 
-    CuMatrix<BaseFloat> w_z_x_corr_;
-    CuMatrix<BaseFloat> w_r_x_corr_;
-    CuMatrix<BaseFloat> w_m_x_corr_;
-    CuMatrix<BaseFloat> w_z_h_corr_;
-    CuMatrix<BaseFloat> w_r_h_corr_;
+    CuMatrix<BaseFloat> w_zrm_x_corr_;
+    CuMatrix<BaseFloat> w_zr_h_corr_;
     CuMatrix<BaseFloat> w_m_g_corr_;
 
     // propagate buffer: output of [z, r, g, m, h]
